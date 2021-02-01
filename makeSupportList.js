86fa168345e7a8d03f082b5f5ace1bec7769dac2
katai5plate <katai5plate.sub@gmail.com>
2021-02-01 17:35:05 +0900

    add {version_added: null}

--- makeSupportList.js
+++ makeSupportList.js
@@ -2,6 +2,11 @@ const bcd = require("@mdn/browser-compat-data");
 
 module.exports = () => {
   const { browsers, ...dataList } = bcd;
+  const browserList = Object.keys(browsers);
+  const initSupport = browserList.reduce(
+    (p, c) => ({ ...p, [c]: { version_added: null } }),
+    {}
+  );
   let result = [];
   const find = (objData, breadcrumb = []) => {
     for (let [name, data] of Object.entries(objData)) {
@@ -14,10 +19,14 @@ module.exports = () => {
       } else {
         const path = [...breadcrumb, name];
         console.log(path.join("/"));
-        result = [...result, { path, support: data.__compat.support }];
+        result = [
+          ...result,
+          { path, support: { ...initSupport, ...data.__compat.support } },
+        ];
       }
     }
   };
   find(dataList);
+  // console.log(Object.keys(browsers));
   return result;
 };
