628d23ddec10b6f0eef94438be6d88f54c7dd436
katai5plate <katai5plate.sub@gmail.com>
2021-02-02 08:35:21 +0900

    add viewer

---
+++ getResult.js
@@ -0,0 +1,25 @@
+const getResult = (
+  { supportList, versionList, browserName, version },
+  type
+) => {
+  const list = versionList[browserName];
+  const versions = [true, ...list.slice(list.findIndex((x) => x === version))];
+  const result = supportList
+    .filter(({ supports }) =>
+      versions.includes(supports[browserName][0].version_added)
+    )
+    .map(({ path, ...v }) => ({ ...v, paths: path.join("/") }));
+  if (type === "html")
+    return result.reduce(
+      (p, { paths, mdn }) => `${p}<p><a href="${mdn}">${paths}</a></p>\n`,
+      ""
+    );
+  if (type === "json") return JSON.stringify(result, null, 2);
+  return result;
+};
+
+try {
+  module.exports = getResult;
+} catch (e) {
+  // ブラウザの場合は定義のみ
+}
--- index.js
+++ index.js
@@ -1,20 +1,6 @@
+const [_, __, browserName, version] = process.argv;
 const supportList = require("./supportList.json");
 const versionList = require("./versionList.json");
-
-const getVersions = (browserName, version) => {
-  const list = versionList[browserName];
-  return [true, ...list.slice(list.findIndex((x) => x === version))];
-};
-
-const BROWSER_NAME = "safari_ios";
-const BROWSER_VERSION = "12";
-
-const versions = getVersions(BROWSER_NAME, BROWSER_VERSION);
-
-supportList
-  .filter(({ supports }) =>
-    versions.includes(supports[BROWSER_NAME][0].version_added)
-  )
-  .forEach(({ path }) => console.log(path.join("/")));
-
-console.log(versions);
+const getResult = require("./getResult");
+const result = getResult({ supportList, versionList, browserName, version });
+console.log(result[0].supports);
