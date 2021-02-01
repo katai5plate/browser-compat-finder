4186e185002db887dbc3963d4a579e0e901273a8
katai5plate <katai5plate.sub@gmail.com>
2021-02-02 04:09:35 +0900

    beta

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const { writeFileSync } = require("fs");
 const supportList = require("./supportList.json");
 const versionList = require("./versionList.json");
+
+const getVersions = (browserName, version) => {
+  const list = versionList[browserName];
+  return [true, ...list.slice(list.findIndex((x) => x === version))];
+};
+
+const BROWSER_NAME = "safari_ios";
+const BROWSER_VERSION = "12";
+
+const versions = getVersions(BROWSER_NAME, BROWSER_VERSION);
+
+supportList
+  .filter(({ supports }) =>
+    versions.includes(supports[BROWSER_NAME][0].version_added)
+  )
+  .forEach(({ path }) => console.log(path.join("/")));
+
+console.log(versions);
